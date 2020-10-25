d5214f583b07ee4d74559739f9a48ced4cfb1407
Matias Cardenas <matias.cardenas@asml.com>
2020-10-26 00:15:08 +0100

    Adding templates to Django first project

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -4,5 +4,9 @@ from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
 
-def index(request: HttpRequest):
-    return HttpResponse("Hello World!")
+# def index(request: HttpRequest):
+#     return HttpResponse("Hello World!")
+
+def index(request: HttpRequest) -> HttpResponse:
+    index_tags = {"insert_me": "Hello I am from views.py"}
+    return render(request, "first_app/index.html", context=index_tags)
