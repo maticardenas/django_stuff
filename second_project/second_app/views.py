e9084a36dc3e027cc63b12845cca5950a32f3081
Matias Cardenas <matias.cardenas@asml.com>
2020-11-04 00:09:59 +0100

    Connecting model / views & templates for the first time

--- second_project/second_app/views.py
+++ second_project/second_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from second_app.models import AccessRecord, Topic, WebPage
 
 # Create your views here.
 def index(request: HttpRequest) -> HttpResponse:
-    index_tags = {"insert_content": "Hello I am from second_app views!"}
-    return render(request, "second_app/index.html", context=index_tags)
+    webpages_list = AccessRecord.objects.order_by("date")
+    date_dict = {"access_records": webpages_list}
+    return render(request, "second_app/index.html", context=date_dict)
+
