7f910e85f80c7f03c6adfbc36e40dc5ac6b784fa
Matias Cardenas <matias.cardenas@asml.com>
2020-11-03 22:25:50 +0100

    First nodels and migrations

--- ProTwo/ProTwo/urls.py
+++ ProTwo/ProTwo/urls.py
@@ -10,15 +10,16 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import the include() function: from django.urls import include, path^
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from AppTwo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^$', views.index, name='index'),
-    re_path(r'help/', views.help, name='help')
+    #re_path(r'help/', views.help, name='help')
+    re_path(r'^help/', include("AppTwo.urls"))
 ]
